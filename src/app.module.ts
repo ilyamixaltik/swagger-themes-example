9098eca1431f2e2f65d4da2500fdfd6ea03b370e
ilyamixaltik <mixaltik16@gmail.com>
2024-02-11 06:04:38 +0300

    created users crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
